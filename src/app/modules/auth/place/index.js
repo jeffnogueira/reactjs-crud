d0a089aad2afe86de285b2d453f77192c78dbac7
jeffnogueira <jefferson.nogueira.0180@gmail.com>
2020-07-30 23:16:35 -0300

    Implement functions to refresh and search in places

--- src/app/modules/auth/place/index.js
+++ src/app/modules/auth/place/index.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CardHeader from '../../../shared/components/card-header';
 import { Container } from './styles';
+import api from "../../../shared/services/api.service";
+import { errorSwalService } from './../../../shared/services/swal.service';
 import PlaceIcon from '@material-ui/icons/Place';
 
-const Home = () => (
-	<Container>
-		<CardHeader icon={PlaceIcon} title="Places" />
-		<div className="card-data-table">
+const Home = () => {
 
-		</div>
-		
-	</Container>
-);
+	const [ places, setPlaces ] = useState('');
+
+	useEffect(() => {
+		onRefresh();
+	}, []);
+    
+    const onRefresh = () => {		
+		api.get("/api/v1.0/places").then(res => {
+			setPlaces(res.data.places)
+			console.log(res.data.places)
+		}).catch(error => {
+			errorSwalService('Ops', error.data.description);
+		});
+    }
+    
+    const onSearch = (value = null) => {		
+		api.get(`/api/v1.0/places/search/${value}`).then(res => {
+			setPlaces(res.data.places)
+		}).catch(error => {
+			errorSwalService('Ops', error.data.description);
+		});
+    }
+	
+	return (
+		<Container>
+			<CardHeader icon={PlaceIcon} title="Places" onSearch={(value) => onSearch(value)}  onRefresh={(value) => onRefresh()} />
+			<div className="card-data-table">
+
+			</div>
+			
+		</Container>
+	)
+};
 
 export default Home;
